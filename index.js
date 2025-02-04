96453b12757a0a75b909b4c71ee16f7d1d657d0e
curtisbonham <curtis.bonham1@gmail.com>
2025-02-04 14:57:50 -0700

    tasks persist after refresh, but checked items become unchecked, trying to figure out this issue

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+const taskInput = document.getElementById("myInput");
+const taskList = document.getElementById("myUL");
+const addBtn = document.getElementById("addBtn");
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
 // finds value of input field and creates new list item
 const newListItem = () =>{
 let li = document.createElement("LI");
-let inputValue = document.getElementById("myInput").value;
+let inputValue = taskInput.value;
 let text = document.createTextNode(inputValue);
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 li.appendChild(text);
 if(inputValue === ""){
-  alert("Your entry is blank! Pleae find something to do!")
+  alert("Your entry is blank! Pleae find something to do!");
+  return;
 } else {
-  document.getElementById("myUL").appendChild(li);
+  taskList.appendChild(li);
+  tasks.push(inputValue);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  displayTasks();
 
   //creates a close button when a new item is added with add button
   let toDoList = document.getElementsByTagName("LI");
+
   for(let i = 0; i < toDoList.length; i++){
   let span = document.createElement("span");
   let closeIcon = document.createTextNode("\u00D7");
@@ -20,14 +29,14 @@ if(inputValue === ""){
   toDoList[i].appendChild(span);
 };
 }
-document.getElementById("myInput").value = "";
-document.getElementById("myInput").focus();
-localStorage.setItem("tasks", JSON.stringify(tasks));
-console.log(document.getElementById("myUL"));
+//clears the input field after submitting new task
+taskInput.value = "";
+
+//puts focus back on input field after submitting new task
+taskInput.focus();
 }
 
 // gives functionality to the add new button
-const addBtn = document.getElementById("addBtn")
 addBtn.addEventListener("click", newListItem);
 
 //allows you to cross off a to-do list item when completed or uncheck
@@ -44,8 +53,43 @@ list.addEventListener("click", (event) => {
   if(event.target.className === "close"){
     const listItem = event.target.parentNode;
     listItem.remove()
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
   }
 });
+
+const addTaskToDOM = (taskText, index) => {
+  let li = document.createElement("LI");
+  li.textContent = taskText;
+  let span = document.createElement("span");
+  let closeIcon = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(closeIcon);
+  li.appendChild(span);
+  taskList.appendChild(li);
+  span.addEventListener("click", () => {
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
+  });
+//   li.addEventListener("click", () => {
+//     li.classList.toggle("checked");
+//   });
+//   const list = document.querySelector("ul");
+//  list.addEventListener("click", (event) => {
+//   if(event.target.tagName === "LI"){
+//     event.target.classList.toggle("checked");
+//   }
+// }, false);
+};
+
+const displayTasks = () => {
+  taskList.innerHTML = "";
+  tasks.forEach((taskText, index) => {
+    addTaskToDOM(taskText, index);
+  });
+
+}
 
 const displayDate = () => {
   let date = new Date()
@@ -55,6 +99,7 @@ const displayDate = () => {
 
 window.onload = () => {
   displayDate()
+  displayTasks()
 }
 
 
