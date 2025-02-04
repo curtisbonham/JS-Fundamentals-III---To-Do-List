7a7d9c34ae95b5910501a3dd29cbe290f4e27dcf
curtisbonham <curtis.bonham1@gmail.com>
2025-02-04 13:46:51 -0700

    added date time feature and focus on input box and started trying to get the list to reamin after refresh

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-// finds value of input field and creates new list item and adds a close button
+// finds value of input field and creates new list item
 const newListItem = () =>{
 let li = document.createElement("LI");
 let inputValue = document.getElementById("myInput").value;
 let text = document.createTextNode(inputValue);
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 li.appendChild(text);
 if(inputValue === ""){
   alert("Your entry is blank! Pleae find something to do!")
 } else {
   document.getElementById("myUL").appendChild(li);
+
+  //creates a close button when a new item is added with add button
   let toDoList = document.getElementsByTagName("LI");
-for(let i = 0; i < toDoList.length; i++){
+  for(let i = 0; i < toDoList.length; i++){
   let span = document.createElement("span");
   let closeIcon = document.createTextNode("\u00D7");
   span.className = "close";
@@ -18,6 +21,9 @@ for(let i = 0; i < toDoList.length; i++){
 };
 }
 document.getElementById("myInput").value = "";
+document.getElementById("myInput").focus();
+localStorage.setItem("tasks", JSON.stringify(tasks));
+console.log(document.getElementById("myUL"));
 }
 
 // gives functionality to the add new button
@@ -33,16 +39,6 @@ list.addEventListener("click", (event) => {
   }
 }, false);
 
-//adds a close button to a list item
-let toDoList = document.getElementsByTagName("LI");
-for(let i = 0; i < toDoList.length; i++){
-  let span = document.createElement("span");
-  let closeIcon = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(closeIcon);
-  toDoList[i].appendChild(span);
-};
-
 //removes list item when the close button (X) is clicked
 list.addEventListener("click", (event) => {
   if(event.target.className === "close"){
@@ -50,6 +46,30 @@ list.addEventListener("click", (event) => {
     listItem.remove()
   }
 });
+
+const displayDate = () => {
+  let date = new Date()
+  date = date.toString().split(" ")
+  document.querySelector("#date").innerHTML = `${date[1]} ${date[2]} ${date[3]}`
+}
+
+window.onload = () => {
+  displayDate()
+}
+
+
+
+// const loadTasks = () => {
+//   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+//   tasks.forEach(task => {
+//     let li = document.createElement('li');
+//     li.textContent = task;
+//     taskList.appendChild(li);
+//   });
+// }
+
+// window.addEventListener('load', loadTasks);
+
 
 
 
